9a056a43edd6af4d64ec226a26829b908cb75589
Eddie-Brewer <eddie.brewer074@students.cmcss.net>
2025-12-18 13:28:42 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,24 @@
 
+slices = party_pizza_mini + large + medium
+print(f"Number of slices:{slices}")
+
+people = people + 1
+share = slices // people
+leftover = slices % people
+print(f'Share: {share}')
+print(f'Leftover: {leftover}')
+
+people = people + 2 #Eric and Brandon are coming too.
+share = slices // people
+leftover = slices % people
+print(f'Share: {share}')
+print(f'Leftover: {leftover}')
+
+#Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
+
+people = people + 1
+share = slices // people
+leftover = slices % people
+print(f'Share: {share}')
+print(f'Leftover:{leftover}')
+print("...for Mr. Hollow Leg")
